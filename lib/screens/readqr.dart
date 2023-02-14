be29eddcdccfd29b4dfc136e61ab4c46a0b5998c
Jubadj <adjaoud.juba.17@gmail.com>
2023-02-14 11:52:52 +0100

    Add return qrstr

--- lib/screens/readqr.dart
+++ lib/screens/readqr.dart
@@ -9,50 +9,47 @@ class ScanScreen extends StatefulWidget {
 
 class _ScanScreenState extends State<ScanScreen> {
   var qrstr = "let's Scan it";
-  var height,width;
+  var height, width;
 
   @override
   Widget build(BuildContext context) {
-
-    height = MediaQuery
-        .of(context)
-        .size
-        .height;
-    width = MediaQuery
-        .of(context)
-        .size
-        .width;
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Scanning QR code'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(qrstr,style: TextStyle(color: Colors.blue,fontSize: 30),),
-            ElevatedButton(onPressed: scanQr, child:
-            Text(('Scanner'))),
-            SizedBox(height: width,)
-          ],
+        appBar: AppBar(
+          title: Text('Scanning QR code'),
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                qrstr,
+                style: TextStyle(color: Colors.blue, fontSize: 30),
+              ),
+              ElevatedButton(onPressed: scanQr, child: Text(('Scanner'))),
+              SizedBox(
+                height: width,
+              )
+            ],
+          ),
+        ));
   }
 
-
-  Future <void>scanQr()async{
-    try{
-      FlutterBarcodeScanner.scanBarcode('#2A99CF', 'cancel', true, ScanMode.QR).then((value){
+  Future<String> scanQr() async {
+    try {
+      FlutterBarcodeScanner.scanBarcode('#2A99CF', 'cancel', true, ScanMode.QR)
+          .then((value) {
         setState(() {
-          qrstr=value;
+          qrstr = value;
         });
       });
-    }catch(e){
+    } catch (e) {
       setState(() {
-        qrstr='unable to read this';
+        qrstr = 'unable to read this';
       });
     }
+    return qrstr;
   }
 }
