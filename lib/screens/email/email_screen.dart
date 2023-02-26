4e5e16e0315b6814abfec5b60d65c116196d931e
Jubadj <adjaoud.juba.17@gmail.com>
2023-02-26 22:01:51 +0100

    Redirection Done from email screen

--- lib/screens/email/email_screen.dart
+++ lib/screens/email/email_screen.dart
@@ -1,5 +1,9 @@
 import 'package:appmobile/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import '../../services/api_service.dart';
+import '../qr_code/qr_code_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,34 +27,54 @@ class EmailScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Bienvenue!',
+            const Text(
+              'Bienvenue',
               style: TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.bold, // mettre le texte en gras
                 color: kTextColor,
               ),
             ),
-            SizedBox(height: 18),
+            const SizedBox(height: 18),
             Container(
               width: 350,
               child: TextField(
                 controller: emailController,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: 'Entrer votre email',
                 ),
               ),
             ),
-            SizedBox(height: 22),
+            const SizedBox(height: 22),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 primary: kPrimaryColor, // changer la couleur de fond du bouton
-                minimumSize: Size(150, 75), // agrandir le bouton
+                minimumSize: const Size(150, 75), // agrandir le bouton
               ),
-              onPressed: () {
-                // TODO: Add email submission logic
+              onPressed: () async {
+                String email = emailController.text;
+                final apiService = ApiService();
+                String response = await apiService.connectionSeller(email);
+                if(response == "connection successful") {
+                Fluttertoast.showToast(
+                    msg: "Vérification réussie, consultez vos mails",
+                    toastLength: Toast.LENGTH_LONG,
+                    gravity: ToastGravity.TOP,
+                    backgroundColor: Colors.green,
+                    textColor: Colors.white,
+                    fontSize: 16.0);
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Qr_code_Screen()));
+                } else {
+                  Fluttertoast.showToast(
+                    msg: "Vérification échouée",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.TOP,
+                    backgroundColor: Color.fromARGB(255, 170, 24, 27),
+                    textColor: Colors.white,
+                    fontSize: 16.0);
+                }
               },
-              child: Text('Entrer'),
+              child: const Text('Entrer'),
             ),
           ],
         ),
