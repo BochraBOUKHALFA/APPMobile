ed8eb2bb1c7060af3c770e304f18bfa4b6cb92c7
kenzabenzenati <benzenatikenza2@gmail.com>
2023-03-08 16:02:10 +0100

    qr code screen updated

--- lib/screens/qr_code/qr_code_screen.dart
+++ lib/screens/qr_code/qr_code_screen.dart
@@ -23,106 +23,114 @@ class _ScanScreenState extends State<Qr_code_Screen> {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Form(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/14.png'),
+            fit: BoxFit.cover,
+            alignment: Alignment.topCenter,
+          ),
+        ),
         key: _formKey,
         child: Center(
-          child: Card(
-            elevation: 10,
-            child: Container(
-              padding: const EdgeInsets.all(50.0),
-              constraints: const BoxConstraints(maxWidth: 500),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 80),
-                    Image.asset(
-                      'assets/images/Mon_projet_12.png',
+          child: Container(
+            padding: const EdgeInsets.all(50.0),
+            constraints: const BoxConstraints(maxWidth: 400),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 40),
+                  Image.asset(
+                    'assets/images/Mon_projet_12.png',
+                  ),
+                  _gap(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      "Scanner le QR Code !",
+                      style: Theme.of(context).textTheme.headline5,
+                      selectionColor: kPrimaryColor,
                     ),
-                    _gap(),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text(
-                        "Scanner le QR Code !",
-                        style: Theme.of(context).textTheme.headline5,
-                        selectionColor: kPrimaryColor,
-                      ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(height: 50),
-                        Container(
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: kPrimaryColor,
-                            borderRadius: BorderRadius.circular(4),
-                          ),
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(4),
-                              ),
-                              primary: Colors.transparent,
-                              shadowColor: Colors.transparent,
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.all(15.0),
-                              child: Text(
-                                'Here',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            onPressed: () {
-                              scanQr().then((qrResult) {
-                                final apiService = ApiService();
-                                apiService
-                                    .getSeller(qrResult)
-                                    .then((first_name) {
-                                  Fluttertoast.showToast(
-                                      msg: "Connexion réussie",
-                                      toastLength: Toast.LENGTH_LONG,
-                                      gravity: ToastGravity.TOP,
-                                      backgroundColor: Colors.green,
-                                      textColor: Colors.white,
-                                      fontSize: 16.0);
-                                  // Redirection vers une autre page si la requête réussit
-                                  Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => HomeScreen()),
-                                  );
-                                }).catchError((error) {
-                                  Fluttertoast.showToast(
-                                      msg: "Connexion echouée",
-                                      toastLength: Toast.LENGTH_LONG,
-                                      gravity: ToastGravity.TOP,
-                                      backgroundColor:
-                                          Color.fromARGB(255, 185, 29, 29),
-                                      textColor: Colors.white,
-                                      fontSize: 16.0);
-                                  // Redirection vers une autre page si la requête échoue
-                                  Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => Qr_code_Screen()),
-                                  );
-                                });
-                              });
-                            },
-                          ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 40),
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: kPrimaryColor,
+                          borderRadius: BorderRadius.circular(4),
                         ),
-                      ],
-                    ),
-                    SizedBox(height: 100),
-                  ],
-                ),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            primary: Colors.transparent,
+                            shadowColor: Colors.transparent,
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Text(
+                              'Ici',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          onPressed: () {
+                            scanQr().then((qrResult) {
+                              final apiService = ApiService();
+                              apiService.getSeller(qrResult).then((first_name) {
+                                Fluttertoast.showToast(
+                                    msg: "Connexion réussie",
+                                    toastLength: Toast.LENGTH_LONG,
+                                    gravity: ToastGravity.TOP,
+                                    backgroundColor: Colors.green,
+                                    textColor: Colors.white,
+                                    fontSize: 16.0);
+                                // Redirection vers une autre page si la requête réussit
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomeScreen()),
+                                );
+                              }).catchError((error) {
+                                Fluttertoast.showToast(
+                                    msg: "Connexion echouée",
+                                    toastLength: Toast.LENGTH_LONG,
+                                    gravity: ToastGravity.TOP,
+                                    backgroundColor:
+                                        Color.fromARGB(255, 185, 29, 29),
+                                    textColor: Colors.white,
+                                    fontSize: 16.0);
+                                // Redirection vers une autre page si la requête échoue
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Qr_code_Screen()),
+                                );
+                              });
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 40),
+                ],
               ),
             ),
           ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Image.asset(
+          'assets/images/13.png',
+          fit: BoxFit.cover,
         ),
       ),
     );
