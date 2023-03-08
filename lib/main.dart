2755c4cd2d12eb09cd8bcb09ecbbfbb52e625800
Jubadj <adjaoud.juba.17@gmail.com>
2023-03-09 00:52:45 +0100

    first test to add 3d object

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:appmobile/constants.dart';
+import 'package:appmobile/screens/details/details_screen.dart';
 import 'package:appmobile/screens/email/email_screen.dart';
 import 'package:appmobile/screens/prehome/prehome_screen.dart';
 import 'package:flutter/material.dart';
