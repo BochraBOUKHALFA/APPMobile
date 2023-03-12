944bce891b41cc040294a05b150c276aba3450c2
small project <boukhalfabouchra101@gmail.com>
2023-03-12 19:59:57 +0100

    errors on build:gradel versions and andriods config files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:appmobile/constants.dart';
 import 'package:appmobile/screens/details/details_screen.dart';
+import 'package:appmobile/screens/details/AR.dart';
 import 'package:appmobile/screens/email/email_screen.dart';
 import 'package:appmobile/screens/prehome/prehome_screen.dart';
 import 'package:flutter/material.dart';
