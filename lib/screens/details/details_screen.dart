60eeb4144f9540f81318dc17fb4bfe8dadc2db65
kenzabenzenati <benzenatikenza2@gmail.com>
2023-03-21 00:29:41 +0100

    added a new colors and changed the postion of the boton

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -36,14 +36,18 @@ class DetailsScreen extends StatelessWidget {
           children: [
             Expanded(
               child: WebviewScaffold(
-                  url: 'https://app.vectary.com/p/4j9qXY8W5J1CQkNUBfXGKZ'),
+                url: 'https://app.vectary.com/p/4j9qXY8W5J1CQkNUBfXGKZ',
+              ),
             ),
-            Text(
-              product.name ?? '',
-              style: TextStyle(
+            Center(
+              child: Text(
+                product.name ?? '',
+                style: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
-                  fontSize: 25),
+                  fontSize: 25,
+                ),
+              ),
             ),
             Expanded(
               child: Padding(
@@ -51,25 +55,12 @@ class DetailsScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    RichText(
-                      text: TextSpan(
-                        style: TextStyle(color: Colors.black),
-                        children: [
-                          TextSpan(
-                            text: 'Color: ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(text: '${product.color ?? ''}'),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 8.0),
                     Card(
                       elevation: 4.0,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(8.0),
                       ),
-                      color: Color(0xFFFFF8DC), // beige color
+                      color: Colors.brown,
                       child: Padding(
                         padding: EdgeInsets.all(16.0),
                         child: Column(
@@ -99,18 +90,20 @@ class DetailsScreen extends StatelessWidget {
                       ),
                     ),
                     SizedBox(height: 16.0),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.brown, // background color
-                        onPrimary: Colors.white, // foreground color
+                    Center(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.brown, // background color
+                          onPrimary: Colors.white, // foreground color
+                        ),
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => ARScreen()),
+                          );
+                        },
+                        child: Text('Réalité augmentée'),
                       ),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => ARScreen()),
-                        );
-                      },
-                      child: Text('Réalité augmentée'),
                     ),
                   ],
                 ),
